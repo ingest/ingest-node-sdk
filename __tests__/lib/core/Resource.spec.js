73bdcc5ae32ce4e2de9b3cef7e6471b9e50151bd
Ashley Hasler <ashley.hasler@redspace.com>
2017-09-27 09:18:49 -0300

    INGEST-3133: Added @return to doc comments

--- __tests__/lib/core/Resource.spec.js
+++ __tests__/lib/core/Resource.spec.js
@@ -21,11 +21,121 @@ describe('Resource Tests', () => {
 
     it('Should accept options as a parameter if passed in', () => {
       const options = {
-        all: 'test'
+        test: 'test'
       }
       const resource = new Resource(options)
 
-      expect(resource.config.all).toEqual('test')
+      expect(resource.config.test).toEqual('test')
+    })
+  })
+
+  describe('Resource::_sendRequest', () => {
+    it('Should return a callback with the data', () => {
+
+    })
+
+    it('Should return a promise with an error', () => {
+
+    })
+
+    it('Should return a promise', () => {
+
+    })
+  })
+
+  describe('Resource::_handleInputError', () => {
+    it('Should ...', () => {
+
+    })
+  })
+
+  describe('Resource:: getAll', () => {
+    it('Should ...', () => {
+
+    })
+  })
+
+  describe('Resource:: getById', () => {
+    it('Should ...', () => {
+
+    })
+  })
+
+  describe('Resource:: getTrashed', () => {
+    it('Should ...', () => {
+
+    })
+  })
+
+  describe('Resource:: getThumbnails', () => {
+    it('Should ...', () => {
+
+    })
+  })
+
+  describe('Resource:: add', () => {
+    it('Should ...', () => {
+
+    })
+  })
+
+  describe('Resource:: update', () => {
+    it('Should ...', () => {
+
+    })
+  })
+
+  describe('Resource:: _updateResource', () => {
+    it('Should ...', () => {
+
+    })
+  })
+
+  describe('Resource:: _updateResourceArray', () => {
+    it('Should ...', () => {
+
+    })
+  })
+
+  describe('Resource:: delete', () => {
+    it('Should ...', () => {
+
+    })
+  })
+
+  describe('Resource:: permanentDelete', () => {
+    it('Should ...', () => {
+
+    })
+  })
+
+  describe('Resource:: _deleteResource', () => {
+    it('Should ...', () => {
+
+    })
+  })
+
+  describe('Resource:: _deleteResourceArray', () => {
+    it('Should ...', () => {
+
+    })
+  })
+
+  describe('Resource:: search', () => {
+    it('Should ...', () => {
+
+    })
+  })
+
+  describe('Resource:: count', () => {
+    it('Should ...', () => {
+
+    })
+  })
+
+  describe('Resource:: trashCount', () => {
+    it('Should ...', () => {
+
     })
   })
 })
