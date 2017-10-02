52f182b19e609b992a47a0001c49c0206c7966e8
Ashley Hasler <ashley.hasler@redspace.com>
2017-10-02 08:17:35 -0300

    INGEST-1394: 100% test coverage for the Resource class

--- __tests__/lib/core/Resource.spec.js
+++ __tests__/lib/core/Resource.spec.js
@@ -1,8 +1,13 @@
-/* eslist-env jest */
+/* eslint-env jest */
+jest.mock('../../../lib/core/Request')
 
 const Resource = require('../../../lib/core/Resource');
 
 describe('Resource Tests', () => {
+  beforeEach(() => {
+    const resource = new Resource()
+  });
+
   describe('Resource::constructor', () => {
     it('Should have a config object upon creation', () => {
       const resource = new Resource()
@@ -30,116 +35,344 @@ describe('Resource Tests', () => {
   })
 
   describe('Resource::_sendRequest', () => {
-    it('Should return a callback with the data', () => {
+
+    it('Should return a success callback with the data', () => {
+      let options = {
+        pass: true,
+        data: {
+          test: 'test'
+        }
+      }
+      let resource = new Resource()
+      resource._sendRequest(options, function(err, res) {
+        expect(err).toBeNull()
+        expect(res.test).toEqual('test')
+      })
 
     })
 
-    it('Should return a promise with an error', () => {
+    it('Should return an error callback', () => {
+      let options = {
+        pass: true,
+        data: {
+          test: 'error'
+        }
+      }
+      let resource = new Resource()
+      resource._sendRequest(options, function(err, res) {
+        expect(res).toBeNull()
+        expect(err.test).toEqual('error')
+      })
 
     })
 
     it('Should return a promise', () => {
-
+      let options = {
+        pass: true,
+        data: {
+          test: 'test'
+        }
+      }
+      let resource = new Resource()
+      let result = resource._sendRequest()
+      expect(result.then).toBeDefined()
     })
   })
 
   describe('Resource::_handleInputError', () => {
-    it('Should ...', () => {
+    it('Should return an error callback with the passed in error message', () => {
+      const resource = new Resource()
+      let error = 'this is an error message'
+      resource._handleInputError(error, function(err, res){
+        expect(err).toEqual(error)
+        expect(res).toBeNull()
+      })
+    })
 
+    it('Should return a rejected promise with the passed in error message', () => {
+      const resource = new Resource()
+      let error = 'this is an error message'
+      let result = resource._handleInputError(error)
+      expect(result.then).toBeDefined()
     })
   })
 
   describe('Resource:: getAll', () => {
-    // it('Should setup callback if no headers are passed in', () => {
-    //   let resource = new Resource()
+    it('Should call _sendRequest if no headers are passed in', () => {
+      let resource = new Resource()
+      resource.getAll(function(err, res){
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
+    })
 
-    //   let passedInCallback = function(){}
-    //   resource.getAll(passedInCallback)
+    it('Should call _sendRequest if null headers are passed in', () => {
+      let resource = new Resource()
+      resource.getAll(null, function(err, res){
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
+    })
 
-    // })
+    it('Should call _sendRequest if no callback is passed in', () => {
+      let resource = new Resource()
+      resource.getAll(null).then(data => {
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
+    })
   })
 
   describe('Resource:: getById', () => {
-    it('Should ...', () => {
+    it('Should call the error handler when no id is passed in', () => {
+      let resource = new Resource()
+      resource.getById().then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
+    })
 
+    it('Should call the error handler when id is not a string', () => {
+      let resource = new Resource()
+      resource.getById({}).then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call _sendRequest when id is passed as a string', () => {
+      let resource = new Resource()
+      resource.getById('test').then(data => {
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
     })
   })
 
   describe('Resource:: getTrashed', () => {
-    it('Should ...', () => {
+    it('Should call _sendRequest if no headers are passed in', () => {
+      let resource = new Resource()
+      resource.getTrashed(function(err, res){
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
+    })
 
+    it('Should call _sendRequest if null headers are passed in', () => {
+      let resource = new Resource()
+      resource.getTrashed(null, function(err, res){
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call _sendRequest if no callback is passed in', () => {
+      let resource = new Resource()
+      resource.getTrashed(null).then(data => {
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
     })
   })
 
   describe('Resource:: getThumbnails', () => {
-    it('Should ...', () => {
+    it('Should call the error handler when no id is passed in', () => {
+      let resource = new Resource()
+      resource.getThumbnails().then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
+    })
 
+    it('Should call the error handler when id is not a string', () => {
+      let resource = new Resource()
+      resource.getThumbnails({}).then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call _sendRequest when id is passed as a string', () => {
+      let resource = new Resource()
+      resource.getThumbnails('test').then(data => {
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
     })
   })
 
   describe('Resource:: add', () => {
-    it('Should ...', () => {
+    it('Should call the error handler when no resource is passed in', () => {
+      let resource = new Resource()
+      resource.add().then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
+    })
 
+    it('Should call the error handler when resource is not an object', () => {
+      let resource = new Resource()
+      resource.add('').then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call _sendRequest when resource is passed as an object', () => {
+      let resource = new Resource()
+      resource.add({test: 'Test Object'}).then(data => {
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
     })
   })
 
   describe('Resource:: update', () => {
-    it('Should ...', () => {
+    it('Should call the error handler when no resource is passed in', () => {
+      let resource = new Resource()
+      resource.update().then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
+    })
 
+    it('Should call the error handler when resource is not an object', () => {
+      let resource = new Resource()
+      resource.update('').then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call _sendRequest when resource is passed as an object', () => {
+      let resource = new Resource()
+      resource.update({test: 'Test Object'}).then(data => {
+        expect(resource._updateResource).toHaveBeenCalled()
+      })
     })
   })
 
   describe('Resource:: _updateResource', () => {
-    it('Should ...', () => {
-
+    it('Should call the error handler when no resource is passed in', () => {
+      let resource = new Resource()
+      resource._updateResource().then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
     })
-  })
 
-  describe('Resource:: _updateResourceArray', () => {
-    it('Should ...', () => {
+    it('Should call the error handler when resource is not an object', () => {
+      let resource = new Resource()
+      resource._updateResource('').then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
+    })
 
+    it('Should call _sendRequest when resource is passed as an object', () => {
+      let resource = new Resource()
+      resource._updateResource({test: 'Test Object'}).then(data => {
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
     })
   })
 
   describe('Resource:: delete', () => {
-    it('Should ...', () => {
+    it('Should call the error handler when no resource is passed in', () => {
+      let resource = new Resource()
+      resource.delete().then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
+    })
 
+    it('Should call the error handler when resource is not a string', () => {
+      let resource = new Resource()
+      resource.delete({}).then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call _sendRequest when resource is passed as a string', () => {
+      let resource = new Resource()
+      resource.delete('resource id').then(data => {
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
     })
   })
 
   describe('Resource:: permanentDelete', () => {
-    it('Should ...', () => {
+    it('Should call the error handler when no resource is passed in', () => {
+      let resource = new Resource()
+      resource.permanentDelete().then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
+    })
 
+    it('Should call the error handler when resource is not a string', () => {
+      let resource = new Resource()
+      resource.permanentDelete({}).then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call _sendRequest when resource is passed as a string', () => {
+      let resource = new Resource()
+      resource.permanentDelete('resource id').then(data => {
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
     })
   })
 
   describe('Resource:: _deleteResource', () => {
-    it('Should ...', () => {
-
+    it('Should call the error handler when resource is not a string', () => {
+      let resource = new Resource()
+      resource._deleteResource({}, true).then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
     })
-  })
 
-  describe('Resource:: _deleteResourceArray', () => {
-    it('Should ...', () => {
+    it('Should call the error handler when permanent is not a boolean', () => {
+      let resource = new Resource()
+      resource._deleteResource('test', 'true').then(data => {
+        expect(resource._handleInputError).toHaveBeenCalled()
+      })
+    })
 
+    it('Should call sendRequest when parameters are passed in successfully', () => {
+      let resource = new Resource()
+      resource._deleteResource('test', true).then(data => {
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
     })
   })
 
   describe('Resource:: search', () => {
-    it('Should ...', () => {
-
+    it('Should call _sendRequest if no headers are passed in', () => {
+      let resource = new Resource()
+      resource.search('input', function(err, res){
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
     })
+
+    it('Should call _sendRequest if null headers are passed in', () => {
+      let resource = new Resource()
+      resource.search('input', null, function(err, res){
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call _sendRequest if no callback is passed in', () => {
+      let resource = new Resource()
+      resource.search('input', null).then(data => {
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call _handleInputError if no input is passed in', () => {
+      let resource = new Resource()
+      resource.search(function(err, res){
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
+    })
+
   })
 
   describe('Resource:: count', () => {
-    it('Should ...', () => {
-
+    it('Should call _sendRequest when count function is invoked', () => {
+      let resource = new Resource()
+      resource.count().then(data => {
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
     })
   })
 
   describe('Resource:: trashCount', () => {
-    it('Should ...', () => {
-
+    it('Should call _sendRequest when trashCount function is invoked', () => {
+      let resource = new Resource()
+      resource.trashCount().then(data => {
+        expect(resource._sendRequest).toHaveBeenCalled()
+      })
     })
   })
 })
