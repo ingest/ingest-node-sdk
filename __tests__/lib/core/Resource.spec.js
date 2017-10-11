4633c2257514cfd20fa20c6fa01f2c2319bfb946
Ashley Hasler <ashley.hasler@redspace.com>
2017-10-11 14:17:29 -0300

    INGEST-3197: Start to add utils tests

--- __tests__/lib/core/Resource.spec.js
+++ __tests__/lib/core/Resource.spec.js
@@ -6,7 +6,9 @@ const Resource = require('../../../lib/core/Resource')
 describe('Resource Tests', () => {
   beforeEach(() => {
     this.resource = new Resource()
-    this.spy = jest.spyOn(this.resource, '_sendRequest')
+    this.spy = jest.spyOn(this.resource, '_sendRequest').mockImplementation(() => {
+      return false;
+    })
   })
 
   afterEach(() => {
@@ -14,6 +16,9 @@ describe('Resource Tests', () => {
   })
 
   describe('Resource::_sendRequest', () => {
+    beforeEach(() => {
+      this.spy.mockRestore()
+    })
     it('Should return a success callback with the data', () => {
       let options = {
         pass: true,
@@ -61,11 +66,14 @@ describe('Resource Tests', () => {
       })
     })
 
-    // it('Should return a rejected promise with the passed in error message', () => {
-    //   let error = 'this is an error message'
-    //   let result = this.resource._handleInputError(error)
-    //   expect(result.then).toBeDefined()
-    // })
+    it('Should return a rejected promise with the passed in error message', () => {
+      let error = 'this is an error message'
+      let result = this.resource._handleInputError(error)
+      expect(typeof result.catch === "function").toBeTruthy()
+      result.catch((err) => {
+        expect(err).toEqual(error)
+      })
+    })
   })
 
   describe('Resource:: getAll', () => {
@@ -81,31 +89,34 @@ describe('Resource Tests', () => {
       })
     })
 
-    // it('Should call _sendRequest if no callback is passed in', () => {
-    //   this.resource.getAll(null).then(data => {
-    //     expect(this.resource._sendRequest).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call _sendRequest if no callback is passed in', () => {
+      this.resource.getAll(null)
+      expect(this.resource._sendRequest).toHaveBeenCalled()
+    })
   })
 
   describe('Resource:: getById', () => {
-    // it('Should call the error handler when no id is passed in', () => {
-    //   this.resource.getById().then(data => {
-    //     expect(this.resource._handleInputError).toHaveBeenCalled()
-    //   })
-    // })
+    beforeEach(() => {
+      jest.spyOn(this.resource, '_handleInputError')
+    })
 
-    // it('Should call the error handler when id is not a string', () => {
-    //   this.resource.getById({}).then(data => {
-    //     expect(this.resource._handleInputError).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call the error handler when no id is passed in', () => {
+      this.resource.getById().catch(error => {
+        expect(this.resource._handleInputError).toHaveBeenCalled()
+        expect(error instanceof Error).toBeTruthy()
+      })
+    })
 
-    // it('Should call _sendRequest when id is passed as a string', () => {
-    //   this.resource.getById('test').then(data => {
-    //     expect(this.resource._sendRequest).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call the error handler when id is not a string', () => {
+      this.resource.getById({}).catch(data => {
+        expect(this.resource._handleInputError).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call _sendRequest when id is passed as a string', () => {
+      this.resource.getById('test')
+      expect(this.resource._sendRequest).toHaveBeenCalled()
+    })
   })
 
   describe('Resource:: getTrashed', () => {
@@ -121,131 +132,151 @@ describe('Resource Tests', () => {
       })
     })
 
-    // it('Should call _sendRequest if no callback is passed in', () => {
-    //   this.resource.getTrashed(null).then(data => {
-    //     expect(this.resource._sendRequest).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call _sendRequest if no callback is passed in', () => {
+      this.resource.getTrashed(null)
+      expect(this.resource._sendRequest).toHaveBeenCalled()
+    })
   })
 
   describe('Resource:: add', () => {
-    // it('Should call the error handler when no resource is passed in', () => {
-    //   this.resource.add().then(data => {
-    //     expect(this.resource._handleInputError).toHaveBeenCalled()
-    //   })
-    // })
+    beforeEach(() => {
+      jest.spyOn(this.resource, '_handleInputError')
+    })
 
-    // it('Should call the error handler when resource is not an object', () => {
-    //   this.resource.add('').then(data => {
-    //     expect(this.resource._handleInputError).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call the error handler when no resource is passed in', () => {
+      this.resource.add().catch(data => {
+        expect(this.resource._handleInputError).toHaveBeenCalled()
+      })
+    })
 
-    // it('Should call _sendRequest when resource is passed as an object', () => {
-    //   this.resource.add({test: 'Test Object'}).then(data => {
-    //     expect(this.resource._sendRequest).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call the error handler when resource is not an object', () => {
+      this.resource.add('').catch(data => {
+        expect(this.resource._handleInputError).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call _sendRequest when resource is passed as an object', () => {
+      this.resource.add({test: 'Test Object'})
+      expect(this.resource._sendRequest).toHaveBeenCalled()
+    })
   })
 
   describe('Resource:: update', () => {
-    // it('Should call the error handler when no resource is passed in', () => {
-    //   this.resource.update().then(data => {
-    //     expect(this.resource._handleInputError).toHaveBeenCalled()
-    //   })
-    // })
+    beforeEach(() => {
+      jest.spyOn(this.resource, '_handleInputError')
+    })
 
-    // it('Should call the error handler when resource is not an object', () => {
-    //   this.resource.update('').then(data => {
-    //     expect(this.resource._handleInputError).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call the error handler when no resource is passed in', () => {
+      this.resource.update().catch(data => {
+        expect(this.resource._handleInputError).toHaveBeenCalled()
+      })
+    })
 
-    // it('Should call _sendRequest when resource is passed as an object', () => {
-    //   this.resource.update({test: 'Test Object'}).then(data => {
-    //     expect(this.resource._updateResource).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call the error handler when resource is not an object', () => {
+      this.resource.update('').catch(data => {
+        expect(this.resource._handleInputError).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call _sendRequest when resource is passed as an object', () => {
+      this.spy = jest.spyOn(this.resource, '_updateResource').mockImplementation(() => {
+        return false;
+      })
+      this.resource.update({test: 'Test Object'})
+      expect(this.resource._updateResource).toHaveBeenCalled()
+    })
   })
 
   describe('Resource:: _updateResource', () => {
-    // it('Should call the error handler when no resource is passed in', () => {
-    //   this.resource._updateResource().then(data => {
-    //     expect(this.resource._handleInputError).toHaveBeenCalled()
-    //   })
-    // })
+    beforeEach(() => {
+      jest.spyOn(this.resource, '_handleInputError')
+    })
 
-    // it('Should call the error handler when resource is not an object', () => {
-    //   this.resource._updateResource('').then(data => {
-    //     expect(this.resource._handleInputError).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call the error handler when no resource is passed in', () => {
+      this.resource._updateResource().catch(data => {
+        expect(this.resource._handleInputError).toHaveBeenCalled()
+      })
+    })
 
-    // it('Should call _sendRequest when resource is passed as an object', () => {
-    //   this.resource._updateResource({test: 'Test Object'}).then(data => {
-    //     expect(this.resource._sendRequest).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call the error handler when resource is not an object', () => {
+      this.resource._updateResource('').catch(data => {
+        expect(this.resource._handleInputError).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call _sendRequest when resource is passed as an object', () => {
+      this.resource._updateResource({test: 'Test Object'})
+      expect(this.resource._sendRequest).toHaveBeenCalled()
+    })
   })
 
   describe('Resource:: delete', () => {
-    // it('Should call the error handler when no resource is passed in', () => {
-    //   this.resource.delete().then(data => {
-    //     expect(this.resource._handleInputError).toHaveBeenCalled()
-    //   })
-    // })
+    beforeEach(() => {
+      jest.spyOn(this.resource, '_handleInputError')
+    })
 
-    // it('Should call the error handler when resource is not a string', () => {
-    //   this.resource.delete({}).then(data => {
-    //     expect(this.resource._handleInputError).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call the error handler when no resource is passed in', () => {
+      this.resource.delete().catch(data => {
+        expect(this.resource._handleInputError).toHaveBeenCalled()
+      })
+    })
 
-    // it('Should call _sendRequest when resource is passed as a string', () => {
-    //   this.resource.delete('resource id').then(data => {
-    //     expect(this.resource._sendRequest).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call the error handler when resource is not a string', () => {
+      this.resource.delete({}).catch(data => {
+        expect(this.resource._handleInputError).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call _sendRequest when resource is passed as a string', () => {
+      this.resource.delete('resource id')
+      expect(this.resource._sendRequest).toHaveBeenCalled()
+    })
   })
 
   describe('Resource:: permanentDelete', () => {
-    // it('Should call the error handler when no resource is passed in', () => {
-    //   this.resource.permanentDelete().then(data => {
-    //     expect(this.resource._handleInputError).toHaveBeenCalled()
-    //   })
-    // })
+    beforeEach(() => {
+      jest.spyOn(this.resource, '_handleInputError')
+    })
 
-    // it('Should call the error handler when resource is not a string', () => {
-    //   this.resource.permanentDelete({}).then(data => {
-    //     expect(this.resource._handleInputError).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call the error handler when no resource is passed in', () => {
+      this.resource.permanentDelete().catch(data => {
+        expect(this.resource._handleInputError).toHaveBeenCalled()
+      })
+    })
 
-    // it('Should call _sendRequest when resource is passed as a string', () => {
-    //   this.resource.permanentDelete('resource id').then(data => {
-    //     expect(this.resource._sendRequest).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call the error handler when resource is not a string', () => {
+      this.resource.permanentDelete({}).catch(data => {
+        expect(this.resource._handleInputError).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call _sendRequest when resource is passed as a string', () => {
+      this.resource.permanentDelete('resource id')
+      expect(this.resource._sendRequest).toHaveBeenCalled()
+    })
   })
 
   describe('Resource:: _deleteResource', () => {
-    // it('Should call the error handler when resource is not a string', () => {
-    //   this.resource._deleteResource({}, true).then(data => {
-    //     expect(this.resource._handleInputError).toHaveBeenCalled()
-    //   })
-    // })
+    beforeEach(() => {
+      jest.spyOn(this.resource, '_handleInputError')
+    })
 
-    // it('Should call the error handler when permanent is not a boolean', () => {
-    //   this.resource._deleteResource('test', 'true').then(data => {
-    //     expect(this.resource._handleInputError).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call the error handler when resource is not a string', () => {
+      this.resource._deleteResource({}, true).catch(data => {
+        expect(this.resource._handleInputError).toHaveBeenCalled()
+      })
+    })
 
-    // it('Should call sendRequest when parameters are passed in successfully', () => {
-    //   this.resource._deleteResource('test', true).then(data => {
-    //     expect(this.resource._sendRequest).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call the error handler when permanent is not a boolean', () => {
+      this.resource._deleteResource('test', 'true').catch(data => {
+        expect(this.resource._handleInputError).toHaveBeenCalled()
+      })
+    })
+
+    it('Should call sendRequest when parameters are passed in successfully', () => {
+      this.resource._deleteResource('test', true)
+      expect(this.resource._sendRequest).toHaveBeenCalled()
+    })
   })
 
   describe('Resource:: search', () => {
@@ -261,32 +292,30 @@ describe('Resource Tests', () => {
       })
     })
 
-    // it('Should call _sendRequest if no callback is passed in', () => {
-    //   this.resource.search('input', null).then(data => {
-    //     expect(this.resource._sendRequest).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call _sendRequest if no callback is passed in', () => {
+      this.resource.search('input', null)
+      expect(this.resource._sendRequest).toHaveBeenCalled()
+    })
 
-    // it('Should call _handleInputError if no input is passed in', () => {
-    //   this.resource.search((err, res) => {
-    //     expect(this.resource._handleInputError).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call _handleInputError if no input is passed in', () => {
+      jest.spyOn(this.resource, '_handleInputError')
+      this.resource.search(null, null, (err, res) => {
+        expect(this.resource._handleInputError).toHaveBeenCalled()
+      })
+    })
   })
 
   describe('Resource:: count', () => {
-    // it('Should call _sendRequest when count function is invoked', () => {
-    //   this.resource.count().then(data => {
-    //     expect(this.resource._sendRequest).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call _sendRequest when count function is invoked', () => {
+      this.resource.count()
+      expect(this.resource._sendRequest).toHaveBeenCalled()
+    })
   })
 
   describe('Resource:: trashCount', () => {
-    // it('Should call _sendRequest when trashCount function is invoked', () => {
-    //   this.resource.trashCount().then(data => {
-    //     expect(this.resource._sendRequest).toHaveBeenCalled()
-    //   })
-    // })
+    it('Should call _sendRequest when trashCount function is invoked', () => {
+      this.resource.trashCount()
+      expect(this.resource._sendRequest).toHaveBeenCalled()
+    })
   })
 })
