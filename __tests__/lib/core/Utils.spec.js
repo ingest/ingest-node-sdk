05b4e3c52db8c711c0fe4e3e74b0913ff8fb9ab7
Ashley Hasler <ashley.hasler@redspace.com>
2017-10-12 11:33:09 -0300

    Ingest-3197: Deduplicate tokens

--- __tests__/lib/core/Utils.spec.js
+++ __tests__/lib/core/Utils.spec.js
@@ -3,6 +3,13 @@ const Utils = require('../../../lib/core/Utils')
 const Config = require('../../../lib/core/Config')
 
 describe('Utils Tests', () => {
+  // This valid tokens expiry is in the year 2999
+  const valid_token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOiIzMjUwMzU5MzYwMCIsInN1YiI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJhZG1pbiI6dHJ1ZX0.SRJ8AvhOJyJPfcl5Aqf8-ZiKVoDy72h0RwJQJzx28nI'; // eslint-disable-line
+  const invalid_token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOiIxNDUwMzY2NzkxIiwic3ViIjoiMTIzNDU2Nzg5MCIsIm5hbWUiOiJKb2huIERvZSIsImFkbWluIjp0cnVlfQ.MGdv4o_sNc84OsRlsitw6D933A3zBqEEacEdp30IQeg';  //eslint-disable-line
+  const malformed_token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UifQ.xuEv8qrfXu424LZk8bVgr9MQJUIrp1rHcPyZw_KSsds' //eslint-disable-line
+  const payload_error_token = 'Bearer test.test'
+  const null_token = 'Bearer test'
+
   describe('Utils::parseTokens', () => {
     it('Should return the parsed string', () => {
       jest.spyOn(Config, 'getHost').mockImplementation(() => {
@@ -40,9 +47,6 @@ describe('Utils Tests', () => {
 
   describe('Utils::parseTokenPayload', () => {
     it('Should return the parsed payload if token is valid', () => {
-      // This valid tokens expiry is in the year 2999
-      const valid_token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOiIzMjUwMzU5MzYwMCIsInN1YiI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJhZG1pbiI6dHJ1ZX0.SRJ8AvhOJyJPfcl5Aqf8-ZiKVoDy72h0RwJQJzx28nI'; // eslint-disable-line
-
       const result = Utils.parseTokenPayload(valid_token)
       expect(result).not.toBeNull()
     })
@@ -53,33 +57,27 @@ describe('Utils Tests', () => {
     })
 
     it('Should return null if the token is invalid', () => {
-      const token = 'Bearer test'
-      const result = Utils.parseTokenPayload(token)
+      const result = Utils.parseTokenPayload(null_token)
       expect(result).toBeNull()
     })
 
     it('Should return null if parsed payload errors', () => {
-      const token = 'Bearer test.test'
-      const result = Utils.parseTokenPayload(token)
+      const result = Utils.parseTokenPayload(payload_error_token)
       expect(result).toBeNull()
     })
   })
 
   describe('Utils::isExpired', () => {
     it('Should return false if the token is still valid', function () {
-      // This valid tokens expiry is in the year 2999
-      const valid_token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOiIzMjUwMzU5MzYwMCIsInN1YiI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJhZG1pbiI6dHJ1ZX0.SRJ8AvhOJyJPfcl5Aqf8-ZiKVoDy72h0RwJQJzx28nI'; // eslint-disable-line
       let result = Utils.isExpired(valid_token)
       expect(result).toEqual(false)
     })
 
     it('Should return true if the token is expired.', function () {
-      const invalid_token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOiIxNDUwMzY2NzkxIiwic3ViIjoiMTIzNDU2Nzg5MCIsIm5hbWUiOiJKb2huIERvZSIsImFkbWluIjp0cnVlfQ.MGdv4o_sNc84OsRlsitw6D933A3zBqEEacEdp30IQeg';  //eslint-disable-line
       expect(Utils.isExpired(invalid_token)).toEqual(true)
     })
 
     it('Should return true if the token does not have an exp.', function () {
-      const malformed_token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UifQ.xuEv8qrfXu424LZk8bVgr9MQJUIrp1rHcPyZw_KSsds' //eslint-disable-line
       expect(Utils.isExpired(malformed_token)).toEqual(true)
     })
 
@@ -87,7 +85,6 @@ describe('Utils Tests', () => {
       this.spy = jest.spyOn(Utils, 'parseTokenPayload').mockImplementation(() => {
         return null
       })
-      const malformed_token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UifQ.xuEv8qrfXu424LZk8bVgr9MQJUIrp1rHcPyZw_KSsds' //eslint-disable-line
       expect(Utils.isExpired(malformed_token)).toEqual(true)
     })
   })
